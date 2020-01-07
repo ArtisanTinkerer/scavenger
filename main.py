520ac020aa4933d2c72bcd8787eea484b467cac8
mick.byrne <phpMick@gmail.com>
2020-01-07 12:49:27 +0000

    Selecting items is working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 
 def load_all_items() -> list:
@@ -8,5 +9,28 @@ def load_all_items() -> list:
     return items
 
 
+def input_integer() -> int:
+    """Obtain an integer from the user"""
+    valid_integer = None
+
+    while valid_integer is None:
+        try:
+            valid_integer = int(input('How many items do you require?'))
+
+        except ValueError:
+            print('Please enter an integer.')
+            continue
+    return valid_integer
+
+
+def select_items(number: int) -> list:
+    """Select random items from the list"""
+    selected_items = random.choices(all_items, k=number_of_items)
+    return selected_items
+
+
 all_items = load_all_items()
-print(all_items)
+number_of_items = input_integer()
+selected_items = select_items(number_of_items)
+
+print(selected_items)
